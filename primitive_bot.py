5f85fc86eb6af86745eb7f7a4e7f40b4239c6d5c
loginforregistration <33337816+loginforregistration@users.noreply.github.com>
2017-11-15 19:00:44 +0300

    Update primitive_bot.py
    update — new functions to simplify coding

--- primitive_bot.py
+++ primitive_bot.py
@@ -5,22 +5,35 @@ from telegram.ext import MessageHandler
 from telegram.ext import RegexHandler
 import sqlite3
 
+DB_FAQ_KAI = "db_001.db"
+
+needed_column = 2
+
+column_index = (needed_column*2)-1
+
 # C:\Users\user\Documents\DB_001
 def start(bot, update):
     # подробнее об объекте update: https://core.telegram.org/bots/api#update
     print(update.message.chat.username)
-    con = sqlite3.connect('db_001.db')
+    con = sqlite3.connect(DB_FAQ_KAI)
     cur = con.cursor()
-    results = search(cur, update, "About_military", "Question")
-    bot.sendMessage(chat_id=update.message.chat_id, text=str(results))
+    results = search(cur, update, "Reply", "About_military", "Question")
+    bot_message(bot, update, str(results))
     con.close()
-    bot.sendMessage(chat_id=update.message.chat_id, text="Введите вопрос")
+    bot_message(bot, update, "Введите вопрос")
 
-def search(cur, update, table, column):
-    cur.execute("SELECT * "
+def search(cur, update, column_reply, table, column):
+    cur.execute("SELECT " + column_reply + " "
                 "FROM " + table + " "
                 "WHERE " + column + " LIKE '%" + str(update.message.text) + "%'")
-    return str(cur.fetchone()).split("'")[3]
+    return fetch1(cur)
+
+def bot_message(bot, update, question):
+    bot.sendMessage(chat_id=update.message.chat_id, text=question)
+
+def fetch1(cur):
+    return str(cur.fetchone())
+    # str(cur.fetchone()).split("'")[column_index]
 
 updater = Updater(token='499411892:AAEUsC0XtVTP-TEn3zFW7yovccDvC4LmJg8')  # тут токен, который выдал вам Ботский Отец!
 
